299e851d0eb5d9b2defa5d236b0cb5e2b665e382
a.bogushev <a.bogushev@tinkoff.ru>
2022-12-21 18:36:21 +0300

    - add e2e test for auth server

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -2,10 +2,12 @@ package server
 
 import (
 	"context"
-	"fmt"
 	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 	"google.golang.org/grpc/test/bufconn"
 	"log"
 	"net"
@@ -20,6 +22,7 @@ import (
 )
 
 var client pb.AuthClient
+var db *sqlx.DB
 
 func initServerAndClient(db *sqlx.DB) {
 	buffer := 101024 * 1024
@@ -53,7 +56,7 @@ func initServerAndClient(db *sqlx.DB) {
 func TestMain(m *testing.M) {
 	var code int
 	testutils.RunWithDBContainer(func(dbUrl string) {
-		db := storage.MustInitDB(context.Background(), dbUrl)
+		db = storage.MustInitDB(context.Background(), dbUrl)
 		initServerAndClient(db)
 		code = m.Run()
 	})
@@ -61,17 +64,44 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
-func TestAuthServer_Register(t *testing.T) {
-	ctx := context.Background()
-	token, err := client.Register(ctx, &pb.AuthData{
-		Login:    "login",
-		Password: "password",
-	})
-	token2, err2 := client.Register(ctx, &pb.AuthData{
-		Login:    "login",
-		Password: "password",
-	})
-	fmt.Println(token, err, token2, err2)
-	fmt.Println(token2, err2)
-	//fmt.Println(token, err)
+func prepare() {
+	db.MustExec("truncate table users")
+}
+
+var testAuthData = &pb.AuthData{
+	Login:    "login",
+	Password: "password",
+}
+
+func TestAuthServer_Register_Success(t *testing.T) {
+	prepare()
+
+	token, err := client.Register(context.Background(), testAuthData)
+	assert.NoError(t, err)
+	assert.True(t, len(token.Token) != 0)
+}
+
+func TestAuthServer_Register_UserAlreadyExist(t *testing.T) {
+	prepare()
+
+	_, _ = client.Register(context.Background(), testAuthData)
+	_, err := client.Register(context.Background(), testAuthData)
+
+	assert.ErrorIs(t, err, status.Error(codes.AlreadyExists, "user already exist"))
+}
+
+func TestAuthServer_Login_Success(t *testing.T) {
+	prepare()
+
+	_, _ = client.Register(context.Background(), testAuthData)
+	token, err := client.Login(context.Background(), testAuthData)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, token.Token)
+}
+
+func TestAuthServer_Login_UserNotFound(t *testing.T) {
+	prepare()
+
+	_, err := client.Login(context.Background(), testAuthData)
+	assert.ErrorIs(t, err, status.Error(codes.NotFound, "user not found"))
 }
